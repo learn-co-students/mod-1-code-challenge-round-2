5c81f6560403518cf40b32bffa43670b957331fc
Brandon Harris <cheeridon@gmail.com>
2017-10-27 11:08:45 -0400

    mod 1 code challenge part 2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,19 @@ class Movie
     @@all
   end
 
+  def watchlistings
+    WatchListing.all.select{|watchlist| watchlist.movie == self}
+  end
+
+  def viewers
+    self.watchlistings.map {|list| list.viewer}
+  end
+
+  def average_rating
+    self.watchlistings.inject(0){|sum_rating, watchlist| sum_rating + watchlist.rating} / self.viewers.size
+  end
+
+  def self.highest_rated
+    #WatchListing.all.
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,13 @@ def reload
   load 'config/environment.rb'
 end
 
+tryhard = Movie.new('try hard')
+me = Viewer.new('me')
+apple = Movie.new('apple')
+brandon = Viewer.new('brandon')
+xhanks = Movie.new('xhanks')
+playlist_x = WatchListing.new(me, xhanks, 1)
+me_playlist = WatchListing.new(me, tryhard, 5)
+brandon_playlist_tryhard = WatchListing.new(brandon, tryhard, 15)
+brandon_playlist_apple = WatchListing.new(brandon, apple, 3)
 Pry.start
